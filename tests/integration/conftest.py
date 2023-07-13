be36fb7e13c244943880f6bce38f9e8112418cb5
arturo-seijas <arturo.seijas@canonical.com>
2023-07-13 17:09:56 +0200

    Fix integration test

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -32,6 +32,6 @@ async def app(ops_test: OpsTest, app_name: str):
         application_name=app_name,
         series="focal",
     )
-    status_name = ops.BlockedStatus.name  # type: ignore[has-type]
+    status_name = ops.WaitingStatus.name  # type: ignore[has-type]
     await ops_test.model.wait_for_idle(status=status_name, raise_on_error=False)
     yield application
