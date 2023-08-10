2399b119dd6b43954e6638b4e30ba8839af48d43
arturo-seijas <arturo.seijas@canonical.com>
2023-08-10 12:43:11 +0200

    Add relation integration tests

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -3,7 +3,6 @@
 
 """Fixtures for the SAML Integrator charm integration tests."""
 
-import json
 from pathlib import Path
 
 import pytest_asyncio
@@ -17,32 +16,6 @@ def app_name_fixture():
     """Provide app name from the metadata."""
     metadata = yaml.safe_load(Path("./metadata.yaml").read_text("utf-8"))
     yield metadata["name"]
-
-
-@pytest_asyncio.fixture(scope="module")
-async def deploy_any_charm(ops_test: OpsTest):
-    """Create an async function to deploy any-charm.
-
-    The function accepts a string as the initial src-overwrite configuration.
-    """
-
-    async def _deploy_any_charm(src_overwrite):
-        """Deploy the any-charm for testing.
-
-        Args:
-            src_overwrite: files to overwrite for testing purposes.
-
-        Yields: the any-charm application
-        """
-        application = await ops_test.model.deploy(
-            "any-charm",
-            application_name="any",
-            channel="beta",
-            config={"src-overwrite": src_overwrite},
-        )
-        yield application
-
-    return _deploy_any_charm
 
 
 @pytest_asyncio.fixture(scope="module")
@@ -61,7 +34,7 @@ async def app(ops_test: OpsTest, pytestconfig: Config, app_name: str):
 
 
 @pytest_asyncio.fixture(scope="module")
-async def any_charm():
+async def any_charm(ops_test: OpsTest):
     """SAML Integrator charm used for integration testing.
 
     Build the charm and deploy it along with Anycharm.
@@ -69,8 +42,15 @@ async def any_charm():
     path_lib = "lib/charms/saml_integrator/v0/saml.py"
     saml_lib = Path(path_lib).read_text(encoding="utf8")
     any_charm_script = Path("tests/integration/any_charm.py").read_text(encoding="utf8")
-    any_charm_src_overwrite = {
+    src_overwrite = {
         "saml.py": saml_lib,
         "any_charm.py": any_charm_script,
     }
-    yield deploy_any_charm(json.dumps(any_charm_src_overwrite))
+    assert ops_test.model
+    application = await ops_test.model.deploy(
+        "any-charm",
+        application_name="any",
+        channel="beta",
+        config={"src-overwrite": src_overwrite},
+    )
+    yield application
