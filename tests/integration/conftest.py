b8ba81435811f1daea35add4c95b7482f279fdc4
arturo-seijas <arturo.seijas@canonical.com>
2023-08-10 12:18:26 +0200

    Add relation integration tests

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -3,6 +3,7 @@
 
 """Fixtures for the SAML Integrator charm integration tests."""
 
+import json
 from pathlib import Path
 
 import pytest_asyncio
@@ -19,15 +20,57 @@ def app_name_fixture():
 
 
 @pytest_asyncio.fixture(scope="module")
+async def deploy_any_charm(ops_test: OpsTest):
+    """Create an async function to deploy any-charm.
+
+    The function accepts a string as the initial src-overwrite configuration.
+    """
+
+    async def _deploy_any_charm(src_overwrite):
+        """Deploy the any-charm for testing.
+
+        Args:
+            src_overwrite: files to overwrite for testing purposes.
+
+        Yields: the any-charm application
+        """
+        application = await ops_test.model.deploy(
+            "any-charm",
+            application_name="any",
+            channel="beta",
+            config={"src-overwrite": src_overwrite},
+        )
+        yield application
+
+    return _deploy_any_charm
+
+
+@pytest_asyncio.fixture(scope="module")
 async def app(ops_test: OpsTest, pytestconfig: Config, app_name: str):
     """SAML Integrator charm used for integration testing.
 
-    Build the charm and deploy it.
+    Build the charm and deploy it along with Anycharm.
     """
-    assert ops_test.model
     charm = pytestconfig.getoption("--charm-file")
+    assert ops_test.model
     application = await ops_test.model.deploy(
         f"./{charm}",
         application_name=app_name,
     )
     yield application
+
+
+@pytest_asyncio.fixture(scope="module")
+async def any_charm():
+    """SAML Integrator charm used for integration testing.
+
+    Build the charm and deploy it along with Anycharm.
+    """
+    path_lib = "lib/charms/saml_integrator/v0/saml.py"
+    saml_lib = Path(path_lib).read_text(encoding="utf8")
+    any_charm_script = Path("tests/integration/any_charm.py").read_text(encoding="utf8")
+    any_charm_src_overwrite = {
+        "saml.py": saml_lib,
+        "any_charm.py": any_charm_script,
+    }
+    yield deploy_any_charm(json.dumps(any_charm_src_overwrite))
--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -30,3 +30,30 @@ async def test_active(ops_test: OpsTest, app: ops.Application):
     assert ops_test.model
     await ops_test.model.wait_for_idle(status=status_name, raise_on_error=True)
     assert app.units[0].workload_status == status_name  # type: ignore
+
+
+@pytest.mark.asyncio
+@pytest.mark.abort_on_fail
+async def test_relation(ops_test: OpsTest, app: ops.Application, any_charm: ops.Application):
+    """Check that the charm is active once related to another charm.
+
+    Assume that the charm has already been built and is running.
+    """
+    await app.set_config(  # type: ignore[attr-defined]
+        {
+            "entity_id": "https://login.staging.ubuntu.com",
+            "fingerprint": (
+                "1c:73:51:f2:23:55:f8:3d:25:7e:65:56:dd:f1:a9:17:fe"
+                ":d4:af:dc:70:d2:a8:11:b3:2f:d2:ea:c4:6d:91:e7"
+            ),
+            "metadata_url": "https://login.staging.ubuntu.com/saml/metadata",
+        }
+    )
+    status_name = ops.ActiveStatus.name  # type: ignore[has-type]
+    assert ops_test.model
+    await ops_test.model.wait_for_idle(status=status_name, raise_on_error=True)
+    assert app.units[0].workload_status == status_name  # type: ignore
+    relation_name = f"{app.name}:saml"
+    await ops_test.model.add_relation(any_charm.name, relation_name)
+    await ops_test.model.wait_for_idle(status=status_name, raise_on_error=True)
+    assert app.units[0].workload_status == status_name  # type: ignore
