d8ff2cd91e8eb3985af9f6cda321436fafcfe24d
arturo-seijas <arturo.seijas@canonical.com>
2023-11-30 11:06:41 +0100

    Some mocks

--- tests/interface/conftest.py
+++ tests/interface/conftest.py
@@ -4,9 +4,12 @@
 """Fixtures for charm-relation-interfaces tests."""
 
 import pytest
+from unittest import mock
 from interface_tester.plugin import InterfaceTester
 
+import ops
 from charm import SamlIntegratorOperatorCharm
+from saml import SamlIntegrator
 
 
 # Interface tests are centrally hosted at https://github.com/canonical/charm-relation-interfaces.
@@ -16,6 +19,14 @@ from charm import SamlIntegratorOperatorCharm
 # https://github.com/canonical/charm-relation-interfaces and change saml's test configuration
 # to include the new identifier/location.
 @pytest.fixture
-def interface_tester(interface_tester: InterfaceTester):
+def interface_tester(interface_tester: InterfaceTester, monkeypatch: pytest.MonkeyPatch):
+
+    saml_integrator = mock.MagicMock(spec=SamlIntegrator)
+    saml_integrator.certificates = []
+    saml_integrator.endpoints = []
+    monkeypatch.setattr(SamlIntegrator, "__init__", saml_integrator)
+    def _on_start_patched(self, _) -> None:
+        self.unit.status = ops.ActiveStatus()
+    monkeypatch.setattr(SamlIntegratorOperatorCharm, "_on_start", _on_start_patched)
     interface_tester.configure(charm_type=SamlIntegratorOperatorCharm)
     yield interface_tester
--- tests/interface/test_saml.py
+++ tests/interface/test_saml.py
@@ -4,9 +4,8 @@ from interface_tester import InterfaceTester
 
 
 def test_saml_v0_interface(interface_tester: InterfaceTester):
-    import pdb; pdb.set_trace()
     interface_tester.configure(
-        repo="https://github.com/arturo-seijas/charm-relation-interfaces.git",
+        repo="https://github.com/arturo-seijas/charm-relation-interfaces",
         branch="add-saml-interface-2",
         interface_name="saml",
         interface_version=0,
