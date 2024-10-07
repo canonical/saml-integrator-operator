24dc1739d5073520e508566f7a7f701521ca5b27
Arturo Seijas <arturo.seijas@canonical.com>
2024-10-07 13:35:02 +0200

    ignore interface test

--- tests/interface/test_saml.py
+++ tests/interface/test_saml.py
@@ -1,8 +1,11 @@
 # Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
+import pytest
 from interface_tester import InterfaceTester
 
 
+# https://github.com/canonical/pytest-interface-tester/issues/27
+@pytest.mark.skip
 def test_saml_v0_interface(interface_tester: InterfaceTester):
     interface_tester.configure(
         interface_name="saml",
