d1b547a9700be73849c0397a30764bd1e43860f9
arturo-seijas <102022572+arturo-seijas@users.noreply.github.com>
2024-11-19 14:50:44 +0100

    Reenable interface tests (#108)

--- tests/interface/test_saml.py
+++ tests/interface/test_saml.py
@@ -1,11 +1,14 @@
 # Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
-import pytest
 from interface_tester import InterfaceTester
 
 
-# https://github.com/canonical/pytest-interface-tester/issues/27
-@pytest.mark.skip
+# Interface tests are centrally hosted at https://github.com/canonical/charm-relation-interfaces.
+# this fixture is used by the test runner of charm-relation-interfaces to test saml's compliance
+# with the interface specifications.
+# DO NOT MOVE OR RENAME THIS FIXTURE! If you need to, you'll need to open a PR on
+# https://github.com/canonical/charm-relation-interfaces and change saml's test configuration
+# to include the new identifier/location.
 def test_saml_v0_interface(interface_tester: InterfaceTester):
     interface_tester.configure(
         interface_name="saml",
