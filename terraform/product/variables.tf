91bded0d00986baa53e6fea3866331a22723c072
Arturo Seijas <arturo.seijas@canonical.com>
2025-04-08 12:00:42 +0200

    Add terraform plans

---
+++ terraform/product/variables.tf
@@ -0,0 +1,24 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+variable "model" {
+  description = "Reference to the k8s Juju model to deploy application to."
+  type        = string
+}
+
+variable "saml_integrator" {
+  type = object({
+    app_name    = optional(string, "saml-integrator")
+    channel     = optional(string, "latest/stable")
+    config      = optional(map(string), {})
+    constraints = optional(string, "arch=amd64")
+    revision    = optional(number)
+    base        = optional(string, "ubuntu@22.04")
+    units       = optional(number, 1)
+  })
+}
+
+variable "saml_offer_consumers" {
+  description = "List of consumers for the SAML offer."
+  type        = list(string)
+}
